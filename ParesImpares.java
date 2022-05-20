8c541a6648527b28eac60e41a33c1d4d73b046f6
JUAN-CAMILOC279 <>
2022-05-20 19:29:40 +0000

    see

---
+++ ParesImpares.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner inp = new Scanner(System.in);
+    System.out.print("In:");
+    double max = inp.nextDouble();
+    
+  }
+}
---
+++ calculadora.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class main{
+  
+  public class main (Sttring[]args){
+
+  Scanner 
+  }
+}
